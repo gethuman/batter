67e681b00103807ffff534124057d1329c276050
Jeff Whelpley <jeff@gethuman.com>
2015-02-11 15:05:12 -0500

    Adding before and after for integration testing

--- build/task.test.js
+++ build/task.test.js
@@ -4,6 +4,7 @@
  *
  * Gulp task for testing
  */
+var Q           = require('q');
 var mocha       = require('gulp-mocha');
 var istanbul    = require('gulp-istanbul');
 
@@ -26,34 +27,55 @@ module.exports = function (gulp, opts) {
 
     // function for running one of the tests
     function runTest(targetCode, testCode, done) {
+        var beforePromise = new Q();
+        if (testCode === opts.intTestCode && opts.intBefore) {
+            beforePromise = opts.intBefore();
+        }
+
         if (useTestCoverage) {
-            gulp.src(targetCode)
-                .pipe(istanbul())
-                .pipe(istanbul.hookRequire())
-                .on('finish', function () {
-                    gulp.src(testCode)
-                        .pipe(mocha({
-                            growl: true,
-                            ui: 'bdd',
-                            reporter: reporter,
-                            timeout: 5000
-                        }))
-                        .pipe(istanbul.writeReports({
-                            dir: opts.testDir + '/coverage',
-                            reportOpts: { dir: opts.testDir + '/coverage' }
-                        }))
-                        .on('end', done);
-                });
+            beforePromise.then(function () {
+                gulp.src(targetCode)
+                    .pipe(istanbul())
+                    .pipe(istanbul.hookRequire())
+                    .on('finish', function () {
+                        gulp.src(testCode)
+                            .pipe(mocha({
+                                growl: true,
+                                ui: 'bdd',
+                                reporter: reporter,
+                                timeout: 5000
+                            }))
+                            .pipe(istanbul.writeReports({
+                                dir: opts.testDir + '/coverage',
+                                reportOpts: { dir: opts.testDir + '/coverage' }
+                            }))
+                            .on('end', function () {
+                                if (opts.intAfter) {
+                                    opts.intAfter();
+                                }
+
+                                done();
+                            });
+                    });
+            }).done();
         }
         else {
-            gulp.src(testCode)
-                .pipe(mocha({
-                    growl: true,
-                    ui: 'bdd',
-                    reporter: reporter,
-                    timeout: 5000
-                }))
-                .on('end', done);
+            beforePromise.then(function () {
+                gulp.src(testCode)
+                    .pipe(mocha({
+                        growl: true,
+                        ui: 'bdd',
+                        reporter: reporter,
+                        timeout: 5000
+                    }))
+                    .on('end', function () {
+                        if (opts.intAfter) {
+                            opts.intAfter();
+                        }
+
+                        done();
+                    });
+            }).done();
         }
     }
 
@@ -67,9 +89,10 @@ module.exports = function (gulp, opts) {
         external:  function (done) {
             runTest(opts.intTargetCode, opts.extTestCode, done);
         },
-        '': function (done) {
+        unit: function (done) {
             runTest(opts.unitTargetCode, opts.unitTestCode, done);
-        }
+        },
+        '': ['unit']
     };
 };
 
